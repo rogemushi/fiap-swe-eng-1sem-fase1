93e33ae3301ddd818f8fbc6f63af7ef7fb4f177b
Roger Viana <70344461+rogemushi@users.noreply.github.com>
2022-09-05 23:54:42 -0300

    Exercício Capitulo 07 - Advanced concepts of algorithms and flowcharts for logic in decision making
    Considere que a aplicação solicite ao usuário o tempo, em anos, (considere o mês comercial de 30 dias para fazer os cálculos) que ele fuma, o valor atual do maço e a quantidade média de maços que ele fuma por dia. A partir dessas informações, efetuaremos o cálculo do montante já gasto por ele e o que ele poderia ter comprado com esse dinheiro.

---
+++ python-program/Trabalho Capítulo 7.py
@@ -0,0 +1,18 @@
+# Nome: Roger Viana Gonçalves de Alencar
+# RM: 97540
+
+# Capitulo 07 - Advanced concepts of algorithms and flowcharts for logic in decision making
+
+anos = int(input("Tempo como fumante (em anos)......:"))
+preco = int(input("Valor do maço....................:"))
+quant = int(input("Quantidade de maços por dia.......:"))
+
+valor = ((anos * 365) * preco * quant)
+
+if valor <= 20000:
+    print("Com o valor de {}, você poderia comprar um motinha vrummm".format(valor))
+elif valor > 20000 and valor <= 50000:
+    print("Com o valor de {}, você poderia comprar uma bela moto".format(valor))
+else:
+    print("Com o valor de {}, você poderia comprar um civic tunado".format(valor))
+ 
