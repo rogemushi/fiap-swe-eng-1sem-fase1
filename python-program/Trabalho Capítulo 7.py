79cfd72acfedb1e772df8d16412cf6a335dda89a
Roger Viana <70344461+rogemushi@users.noreply.github.com>
2022-09-06 00:05:06 -0300

    Update Trabalho Capítulo 7.py

--- python-program/Trabalho Capítulo 7.py
+++ python-program/Trabalho Capítulo 7.py
@@ -7,7 +7,7 @@ anos = float(input("Tempo como fumante (em anos)......:"))
 preco = float(input("Valor do maço....................:"))
 quant = float(input("Quantidade de maços por dia.......:"))
 
-valor = ((anos * 365) * preco * quant)
+valor = (((anos * 365) * preco ) * quant)
 
 if valor <= 20000:
     print("Com o valor de {}, você poderia comprar um motinha vrummm".format(valor))
